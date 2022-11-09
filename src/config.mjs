cb42a28abf98d48e052bf341bd8aee88b9bd1157
Damien Wright <dww510@gmail.com>
2022-11-09 13:58:54 -0800

    first commit

---
+++ src/config.mjs
@@ -0,0 +1,37 @@
+export const SITE = {
+	name: 'betahero',
+
+	origin: 'https://betahero.org',
+	basePathname: '/',
+
+	title: 'betahero | inspire, educate and mentor through technology for the heroes of tomorrow',
+	description: 'a nonprofit designed to inspire, educate and mentor youth through technology for the heroes of tomorrow',
+
+	googleAnalyticsId: false, // or "G-XXXXXXXXXX",
+	googleSiteVerificationId: 'orcPxI47GSa-cRvY11tUe6iGg2IO_RPvnA1q95iEM3M',
+};
+
+export const BLOG = {
+	disabled: false,
+	postsPerPage: 4,
+
+	blog: {
+		disabled: false,
+		pathname: 'blog', // blog main path, you can change this to "articles" (/articles)
+	},
+
+	post: {
+		disabled: false,
+		pathname: '', // empty for /some-post, value for /pathname/some-post 
+	},
+
+	category: {
+		disabled: false,
+		pathname: 'category', // set empty to change from /category/some-category to /some-category
+	},
+
+	tag: {
+		disabled: false,
+		pathname: 'tag', // set empty to change from /tag/some-tag to /some-tag
+	},
+};
